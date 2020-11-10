5669cbddf4ec3faba144334d284fd514040143e2
Dylan Eck <dylaneck@mymail.mines.edu>
2020-11-09 17:39:15 -0700

    initial commit
    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config();
+
+client.login(process.env.BOT_TOKEN);
+
+client.on('ready', () => {
+    console.log(`logged in as ${client.user.tag}`);
+});
+
+client.on('message', (msg) =>{
+    if(msg.content.toLowerCase().includes('meme')) {
+        msg.channel.send('REVIEW');
+    }
+});
