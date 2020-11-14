621e0c2793d33a8f12859f05a450f7c66e0099de
Dylan Eck <dylaneck@mymail.mines.edu>
2020-11-14 15:07:13 -0700

    added REVIEW message response

--- python/main.py
+++ python/main.py
@@ -12,4 +12,13 @@ client = discord.Client()
 async def on_ready():
     print(f'connected to discord as {client.user}')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if "meme" in message.content.lower():
+        response = "REVIEW"
+        await message.channel.send(response)
+
 client.run(TOKEN)
