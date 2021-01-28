01e674e8ecf9f2e31a9c07e58882325b0471ea26
Will <willnhemphill@gmail.com>
2021-01-27 17:54:03 -0700

    fixed easy_push.sh, sorry for the last commits

--- easy_push.sh
+++ easy_push.sh
@@ -7,7 +7,8 @@ read -p "[ENTER TO CONTINUE OR CTRL-C]" temp
 echo ">Git Adding All"
 git add --all
 echo ">Git Commmiting..."
-git commit -m %1
+read -p "Comment: " comment
+git commit -m "$comment"
 echo ">Git Pushing!"
 read -p "[ENTER TO CONTINUE OR CTRL-C]" temp
 git push
