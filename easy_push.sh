dad84e4db1691708ff8ec2c66aad87750b415d1c
Will <willnhemphill@gmail.com>
2021-01-27 17:52:00 -0700

    %1

--- easy_push.sh
+++ easy_push.sh
@@ -1,10 +1,13 @@
+echo "Current Git-Status:"
+git status
+echo
 echo "Current Branch:"
 echo $(git branch)
 read -p "[ENTER TO CONTINUE OR CTRL-C]" temp
 echo ">Git Adding All"
 git add --all
 echo ">Git Commmiting..."
-git commit -m "Quick Push"
+git commit -m %1
 echo ">Git Pushing!"
 read -p "[ENTER TO CONTINUE OR CTRL-C]" temp
 git push
